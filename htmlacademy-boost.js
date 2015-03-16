918fa03addcde8533d8647b9ffd56e82e91276bf
Eimrine's Point of View <eimrine@users.noreply.github.com>
2015-03-16 10:09:25 +0200

    Update htmlacademy-boost.js
    monkey patch

--- htmlacademy-boost.js
+++ htmlacademy-boost.js
@@ -1,1 +1,1 @@
-$(function(){if ($(".task-completed").css("display")==="block"){$("#next-lab")[0].click();}})
+$(function check(){setTimeout(function() { check(); }, 1000); if ($(".task-completed").css("display")==="block"){$("#next-lab")[0].click();}})
